81b8313f612dcf4f9e109374907c75354a19c317
Chenchal Subraveti <chenchals@gmail.com>
2019-07-03 08:30:55 -0500

    Updated mainLoop/rezToPhy.m

--- mainLoop/rezToPhy.m
+++ mainLoop/rezToPhy.m
@@ -161,22 +161,21 @@ if ~isempty(savePath)
     fclose(fileIDA);
 
      %make params file
-    if ~exist(fullfile(savePath,'params.py'),'file')
-        fid = fopen(fullfile(savePath,'params.py'), 'w');
-        
-        [~, fname, ext] = fileparts(rez.ops.fbinary);
-        
-        fprintf(fid,['dat_path = ''',fname ext '''\n']);
-        fprintf(fid,'n_channels_dat = %i\n',rez.ops.NchanTOT);
-        fprintf(fid,['dtype = ' rez.ops.dataTypeString '\n']);         
-        fprintf(fid,['offset = ' rez.ops.headerBytes '\n']);
-        if mod(rez.ops.fs,1)
-            fprintf(fid,'sample_rate = %i\n',rez.ops.fs);
-        else
-            fprintf(fid,'sample_rate = %i.\n',rez.ops.fs);
-        end
-        fprintf(fid,'sample_rate_fractional = %.4f\n',rez.ops.fs);        
-        fprintf(fid,'hp_filtered = False');
-        fclose(fid);
-    end
+     if ~exist(fullfile(savePath,'params.py'),'file')
+         fid = fopen(fullfile(savePath,'params.py'), 'w');
+         fprintf(fid,'dat_path = ''%s''\n',rez.ops.fbinary);
+         fprintf(fid,'n_channels_dat = %i\n',rez.ops.NchanTOT);
+         %fprintf(fid,'dtype = ''int16''\n');
+         fprintf(fid,'dtype = ''%s''\n',rez.ops.dataTypeString);
+         %fprintf(fid,'offset = 0\n');
+         fprintf(fid,'offset = %i\n',rez.ops.headerBytes);
+         if mod(rez.ops.fs,1)
+             fprintf(fid,'sample_rate = %i\n',rez.ops.fs);
+         else
+             fprintf(fid,'sample_rate = %i.\n',rez.ops.fs);
+         end
+         fprintf(fid,'sample_rate_fractional = %.4f\n',rez.ops.fs);
+         fprintf(fid,'hp_filtered = False');
+         fclose(fid);
+     end
 end
